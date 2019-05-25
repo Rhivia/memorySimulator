069246d7fbc75901fde6aedb74c067bb7eff1faf
Martin Alexander Kretzschmar <alexander.martin64@gmail.com>
2019-05-25 16:38:17 -0300

    Revert "Updated node exhibition and node removal from list"
    This reverts commit 0aed9ac3b08db37195ade87d70001041c94b19c8.

--- memorySimulator/src/memorysimulator/MemorySimulator.java
+++ memorySimulator/src/memorysimulator/MemorySimulator.java
@@ -1,202 +1,82 @@
-﻿public class listaLigada {
-    private No inicio;// endereço inicial da lista
+package memorysimulator;
 
-    // Construtor para a LISTA LIVRE
-    public listaLigada(int size){
-        // CONSTRUTOR: No(int tamanho, No prox, int endereco)
-        No novo = new No(size, 0);
-        this.inicio = novo;
-    }
+import java.util.Scanner;
 
-    // Construtor para a LISTA ALOCADA
-    public listaLigada(){
-        // CONSTRUTOR: No(int ID, int tamanho, No prox, int endereco)
-        this.inicio = null;
-    }
+public class MemorySimulator {
+    public static listaLigada listaLivre, listaAlocada;
+    
+    public static void main(String[] args) {
+        int processID, maximumSize = 4000, choice;
+        boolean running = false;
+        
+        System.out.println("----------------------------------");
+        System.out.println("     ALOCADOR DE MEMÓRIA 2020");
+        System.out.println("----------------------------------");
+        System.out.println("");
+        
+        listaLivre = new listaLigada(maximumSize);
+        listaAlocada = new listaLigada();
+        Scanner sc = new Scanner(System.in);
+        
+        // getInicio() retorna o PRIMEIRO No da lista
+        // getProx() retorna o PRIMEIRO No da lista
+        
+        running = true;
+        processID = 1; // Começo dos processos, IDs serão incrementais
+        
+        while(running) {
+            System.out.println("----------------------------------------");
+            System.out.println("- 1 - Para adicionar um processo.");
+            System.out.println("- 2 - Para remover um processo.");
+            System.out.println("- 3 - Para encerrar o programa.");
+            choice = sc.nextInt();
 
-    public No getInicio() {
-        return inicio;
-    }
-
-    // Para exeibição dos nós
-    public String toStringAlocada() {
-        System.out.println("Processos: ");
-        return this.inicio.toStringAlocada() + "";
-    }
-
-    // Para exeibição dos nós
-    public String toStringLivre() {
-        System.out.println("Memórias livres: ");
-        return this.inicio.toStringLivre() + "";
-    }
-
-    // Verifica se esta vazia
-    public boolean isEmpty(){
-        return this.inicio == null;
-    }
-
-    // Verifica se esta cheia
-    public boolean isFull(){
-        No aux = this.inicio;
-        No ant = null;
-        int soma = 0;
-
-        while( aux != null && soma <= aux.getTamanho() ){
-            soma += aux.getEndereco();
-            ant = aux;
-            aux = aux.getProx();
-
-            if(soma >= aux.getTamanho()) {
-                return true;
+            switch (choice) {
+                case 1:
+                    System.out.printf("- Qual o tamanho do processo que será iniciado?");
+                    int memory = sc.nextInt();
+                    insert(processID, memory);
+                    processID++;
+                    break;
+                case 2:
+                    System.out.printf("- Qual a indentificação do processo a ser removido?");
+                    int ID = sc.nextInt();
+                    remove(ID);
+                    break;
+                case 3:
+                    running = false;
+                    break;
+                default:
+                    System.out.println("- Código não reconhecido.");
+                    break;
             }
+            tick();
         }
-
-        return false;
     }
-
-
-    // Adiciona No a LISTA LIVRE, chamada somente quando um processo é finalizado
-    // para separar os Nos de memória
-
-
-    // Adiciona No a LISTA ALOCADA
-    public listaLigada addAlocada(int noID, int tamanho, listaLigada listaLivre) {
-        // LISTA LIVRE é recebida como parametro
-        No aux = this.inicio;
-        No atual = null;
-
-        while( aux != null ){
-            atual = aux; // Salva o No anterior
-            aux = aux.getProx(); // Pega o próximo No
-        }
-
-        // No(int ID, int tamanho, No prox, int endereco) {
-        No novo = new No(noID, tamanho, listaLivre.getInicio().getEndereco());
-
-        // Função de loop na lista livre, aloca o processo no menor No que comporte o processo
-        listaLivre.updateNo(tamanho);
-
-        if( atual == null ) {
-            this.inicio = novo;
+    
+    public static void tick () {
+        System.out.println(listaAlocada);
+    }
+    
+    public static void insert (int processID, int memory) {
+        if ( listaLivre.availableSpace() > 0 ) {
+            listaLivre = listaAlocada.addAlocada(processID, memory, listaLivre);
+            
+            System.out.println("- Processo: " + processID + " alocado com sucesso.");
+            System.out.println("----------------------------------------");
+            System.out.println("- Espaço disponível: " + listaLivre.availableSpace());
+            System.out.println("- Espaço alocado: " + listaAlocada.usedSpace());
+            System.out.println("----------------------------------------");
         } else {
-            atual.setProx(novo);
-        }
-
-        return listaLivre;
-    }
-
-    // Função a ser executada na LISTA LIVRE, encontra e atualiza o endereço do
-    // menor No que comporta o processo.
-    public void updateNo (int tamanho) {
-        No aux = this.inicio;
-        No selected = this.inicio;
-
-        while( aux != null ){
-            if (aux.getTamanho() >= tamanho && selected.getTamanho() > aux.getTamanho()) {
-                selected = aux; // Selectiona o No que tem o tamanho apropriada para o processo
-            }
-
-            aux = aux.getProx(); // Pega o próximo No
-        }
-
-        // Atualiza o endereço do No da LISTA LIVRE
-        selected.setEndereco(selected.getEndereco() + tamanho);
-        // Atualiza o tamanho do No da LISTA LIVRE
-        selected.setTamanho(selected.getTamanho() - tamanho);
-
-        if (selected.getTamanho() == 0) {
-            selected = selected.getProx(); // Atualiza a referência para o próximo No
+            System.out.println("- Memória cheia. É necessário remover um processo.");
+            System.out.println("----------------------------------------");
         }
     }
-
-    // Adiciona um No em uma posição de maneira ordenada na lista
-    // A ser executada na LISTA LIVRE
-    public void addOrdenado(No no) {
-        No aux = this.inicio;
-        No atual = null;
-
-        while( aux != null && aux.getEndereco() < no.getEndereco() ){
-            atual = aux;
-            aux = aux.getProx();
-        }
-
-        // No(No prox, int tamanho, int endereco)
-        No novo = new No(aux, no.getTamanho(), no.getEndereco());
-
-        if( atual == null )
-            this.inicio = novo;
-        else
-            atual.setProx(novo);
+    
+    public static void remove (int ID) {
+        No no = listaAlocada.buscaID(ID);
+        System.out.println("No: " + no.getID() + ", no endereço: " + no.getEndereco() + ", tamanho: " + no.getTamanho());
+        System.out.println("Começo da lista livre: " + listaLivre.getInicio().getEndereco());
+        listaLivre.addOrdenado(no);
     }
-
-    // Metódo para ser utilizado na LISTA ALOCADA
-    public int usedSpace() {
-        No aux = this.inicio;
-        int usedSpace = 0;
-
-        while( aux != null ){
-            usedSpace += aux.getTamanho(); // Retorna o tamanho do No
-            aux = aux.getProx(); // Pega o próximo No
-        }
-
-        return usedSpace;
-    }
-
-    // Metódo para ser utilizado na LISTA LIVRE
-    public int availableSpace() {
-        No aux = this.inicio;
-        int availableSpace = 0;
-
-        while( aux != null ){
-            availableSpace += aux.getTamanho(); // Retorna o tamanho do No
-            aux = aux.getProx(); // Pega o próximo No
-        }
-
-        return availableSpace;
-    }
-
-    // Função para ser utilizada na LISTA ALOCADA
-    public No buscaERemove(int ID) {
-        No aux = this.inicio;
-        No atual = null;
-
-        // Busca a ID e sobrescreve o No pelo próximo No da Lista, efetivamente apagando-o
-        while( aux != null ){
-            if( aux.getID() == ID) {
-                atual.setProx(aux.getProx()); // Selectiona o No que tem o tamanho apropriada para o processo
-                return aux;
-            }
-            atual = aux;
-            aux = aux.getProx(); // Pega o próximo No
-        }
-        return null;
-    }
-
-    // Método para ser utilizado na LISTA ALOCADA
-//    public void endereco () {
-//        No aux = this.inicio;
-//        No atual = null;
-//
-//        while( aux != null ){
-//            atual = aux; // Salva o No anterior
-//            System.out.println(atual.getEndereco());
-//            aux = aux.getProx(); // Pega o próximo No
-//        }
-//    }
-//
-//    // versao recursiva
-//    public boolean buscaRec(int x ){
-//        return busca(x, this.inicio);
-//    }
-//
-//    private boolean busca(int target, No aux){
-//        // condicoes de parada
-//        if( aux == null )
-//            return false;
-//
-//        if( aux.getID() == target)
-//            return true;
-//
-//        return busca(target, aux.getProx());
-//    }
 }
--- memorySimulator/src/memorysimulator/No.java
+++ memorySimulator/src/memorysimulator/No.java
@@ -1,33 +1,35 @@
-﻿public class No {
+package memorysimulator;
+
+public class No {
     private int ID;
     private int endereco = 0;
     public int tamanho;
-
+    
     private No prox;
 
     // No inicial para LISTA LIVRE
     public No(int tamanho, int endereco) {
         this.ID = -1; // Nós da lista livre não possuem ID, usaremos -1 para isso.
         this.prox = null; // Como o construtor iniciará a lista, o próximo No é nulo.
-
+        
         this.tamanho = tamanho;
         this.endereco = endereco; // Ao realizar o primeiro No, ele começaré com endereço 0.
     }
-
+    
     // No inicial para LISTA ALOCADA
     // Adicionamos um No para o final da lista alocada, o proximo sempre será nulo
     public No(int ID, int tamanho, int endereco) {
         this.ID = ID; // ID incremental
         this.prox = null;
-
+        
         this.tamanho = tamanho;
         this.endereco = endereco;
     }
-
+    
     public No(No prox, int tamanho, int endereco) {
         this.ID = -1; // ID incremental
         this.prox = prox;
-
+        
         this.tamanho = tamanho;
         this.endereco = endereco;
     }
@@ -35,7 +37,7 @@
     public int getTamanho() {
         return tamanho;
     }
-
+    
     public void setTamanho(int tamanho) {
         this.tamanho = tamanho;
     }
@@ -43,7 +45,7 @@
     public int getEndereco() {
         return endereco;
     }
-
+    
     public void setEndereco(int endereco) {
         this.endereco = endereco;
     }
@@ -59,22 +61,16 @@
     public int getID() {
         return ID;
     }
-
+    
     public int getNo() {
         return this.endereco;
     }
 
-    public String toStringAlocada() {
+    @Override
+    public String toString() {
         if (this.prox == null)
             return "Processo ID: " + this.ID + " esta utilizando " + this.tamanho + " de memória. No endereço " + this.endereco;
         else
-            return "Processo ID: " + this.ID + " esta utilizando " + this.tamanho + " de memória. No endereço " + this.endereco + "\n" + this.prox.toStringAlocada();
-    }
-
-    public String toStringLivre() {
-        if (this.prox == null)
-            return "Bloco de memória contendo " + this.tamanho + " de memória. Endereço: " + this.getEndereco();
-        else
-            return "Bloco de memória contendo " + this.tamanho + " de memória. Endereço: " + this.getEndereco() +  "\n" + this.prox.toStringLivre();
+            return "Processo ID: " + this.ID + " esta utilizando " + this.tamanho + " de memória. No endereço " + this.endereco + "\n" + this.prox;
     }
 }
